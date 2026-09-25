a285935785bb1b0536b3d5ef45b25b8df45bd442
yawgad23 <2.82677219e+08+yawgad23@users.noreply.github.com>
2026-09-25 06:07:06 +0000

    build(driver): reserve iOS build 48

--- app.config.ts
+++ app.config.ts
@@ -31,7 +31,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
-    buildNumber: "46",
+    buildNumber: "48",
     jsEngine: "jsc",
     googleServicesFile: "./firebase/GoogleService-Info.plist",
     ...(iosGoogleMapsApiKey ? { config: { googleMapsApiKey: iosGoogleMapsApiKey } } : {}),
